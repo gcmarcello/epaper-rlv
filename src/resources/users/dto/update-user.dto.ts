d43df791420da31bff943be023e45abb197afbe7
Marcello Coelho <marcellogcoelho@outlook.com>
2024-11-20 21:05:38 +0000

    refactor: remove updateUserDto and its associated type definition

--- src/resources/users/dto/update-user.dto.ts
+++
@@ -1,6 +0,0 @@
-import { z } from "zod";
-import { createUserDto } from "./create-user.dto";
-
-export const updateUserDto = createUserDto.merge(z.object({ id: z.string().uuid() }));
-
-export type UpdateUserDto = z.infer<typeof createUserDto>;
