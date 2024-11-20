d79ea97fd9b4ae73108ea565fe23094a56ddc04e
Marcello Coelho <marcellogcoelho@outlook.com>
2024-11-20 22:35:19 +0000

    feat: add organization_id reference to files entity for improved data integrity

--- src/resources/files/entities/file.entity.ts
+++ src/resources/files/entities/file.entity.ts
@@ -1,6 +1,6 @@
+import { organizations, users } from "@/common/db/db.schema";
 import { InferSelectModel } from "drizzle-orm";
 import { pgTable, serial, text, timestamp, uuid } from "drizzle-orm/pg-core";
-import { users } from "src/resources/users/entities/user.entity";
 
 export const files = pgTable("files", {
   id: serial("id").primaryKey(),
@@ -8,6 +8,9 @@ export const files = pgTable("files", {
   user_id: uuid("user_id")
     .notNull()
     .references(() => users.id),
+  organization_id: uuid("organization_id")
+    .notNull()
+    .references(() => organizations.id),
   created_at: timestamp("created_at").defaultNow(),
   updated_at: timestamp("updated_at").defaultNow(),
 });
