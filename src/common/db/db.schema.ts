b1d9963ae27a8e802ccd29fb0cb20147bbbb5c70
Marcello Coelho <marcellogcoelho@outlook.com>
2024-11-20 22:32:57 +0000

    refactor: update import paths to use alias for improved readability

--- src/common/db/db.schema.ts
+++ src/common/db/db.schema.ts
@@ -3,11 +3,11 @@ import {
   organizationsRelations,
   userOrganizations,
   userOrganizationsRelations,
-} from "../../resources/organizations/entities/organizations.entity";
+} from "@/resources/organizations/entities/organizations.entity";
 
-import { files } from "../../resources/files/entities/file.entity";
+import { files } from "@/resources/files/entities/file.entity";
 
-import { users, usersRelations } from "../../resources/users/entities/user.entity";
+import { users, usersRelations } from "@/resources/users/entities/user.entity";
 
 export {
   users,
