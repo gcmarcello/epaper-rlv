ae85e87064c54124c76660089a1f055688f959b1
Marcello Coelho <marcellogcoelho@outlook.com>
2024-11-20 21:05:28 +0000

    feat: integrate Swagger for API documentation with OpenAPI generation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { SwaggerModule } from "@nestjs/swagger";
+import { generateOpenApi } from "@ts-rest/open-api";
+import { userContract } from "./resources/users/users.contract";
+import { organizationContract } from "./resources/organizations/organizations.contract";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const document = generateOpenApi(
+    { users: userContract, organizations: organizationContract },
+    {
+      info: {
+        title: "EPaper API",
+        version: "1.0.0",
+        description: "API para teste técnico da RLV tecnologia.",
+      },
+    }
+  );
+
+  SwaggerModule.setup("api-docs", app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
