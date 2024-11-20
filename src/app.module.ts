8a99725cac898ccf9d7d6badda9476ae8440b986
Marcello Coelho <marcellogcoelho@outlook.com>
2024-11-20 17:47:34 +0000

    feat: add OrganizationsModule with controller, service, and DTOs for organization management

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { FilesModule } from "./resources/files/files.module";
 import { DBModule } from "./common/db/db.module";
 import { ConfigModule } from "@nestjs/config";
 import { UsersModule } from "./resources/users/users.module";
+import { OrganizationsModule } from "./resources/organizations/organizations.module";
 
 @Module({
-  imports: [ConfigModule.forRoot(), DBModule, FilesModule, UsersModule],
+  imports: [ConfigModule.forRoot(), DBModule, FilesModule, UsersModule, OrganizationsModule],
   controllers: [AppController],
   providers: [AppService],
 })
