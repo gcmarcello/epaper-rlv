1985af42469e1cf4b51e7227865f8c6cf1509899
Marcello Coelho <marcellogcoelho@outlook.com>
2024-11-20 17:45:38 +0000

    feat: add UsersModule with controller, service, and DTOs for user management

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from "./app.service";
 import { FilesModule } from "./resources/files/files.module";
 import { DBModule } from "./common/db/db.module";
 import { ConfigModule } from "@nestjs/config";
+import { UsersModule } from "./resources/users/users.module";
 
 @Module({
-  imports: [ConfigModule.forRoot(), FilesModule, DBModule],
+  imports: [ConfigModule.forRoot(), DBModule, FilesModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
