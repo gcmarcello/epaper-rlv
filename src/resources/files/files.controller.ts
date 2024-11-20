f68f280917ee3d2b0ca633720a86671702e2ab1b
Marcello Coelho <marcellogcoelho@outlook.com>
2024-11-20 23:04:46 +0000

    refactor: remove UpdateFileDto and related update/remove methods from FilesController and FilesService

--- src/resources/files/files.controller.ts
+++ src/resources/files/files.controller.ts
@@ -1,7 +1,6 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from "@nestjs/common";
+import { Controller, Get, Post, Body, Param } from "@nestjs/common";
 import { FilesService } from "./files.service";
 import { CreateFileDto } from "./dto/create-file.dto";
-import { UpdateFileDto } from "./dto/update-file.dto";
 
 @Controller("files")
 export class FilesController {
@@ -20,15 +19,5 @@ export class FilesController {
   @Get(":id")
   findOne(@Param("id") id: string) {
     return this.filesService.findOne(+id);
-  }
-
-  @Patch(":id")
-  update(@Param("id") id: string, @Body() updateFileDto: UpdateFileDto) {
-    return this.filesService.update(+id, updateFileDto);
-  }
-
-  @Delete(":id")
-  remove(@Param("id") id: string) {
-    return this.filesService.remove(+id);
   }
 }
--- src/resources/files/files.service.ts
+++ src/resources/files/files.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from "@nestjs/common";
 import { CreateFileDto } from "./dto/create-file.dto";
-import { UpdateFileDto } from "./dto/update-file.dto";
 
 @Injectable()
 export class FilesService {
@@ -14,10 +13,6 @@ export class FilesService {
 
   findOne(id: number) {
     return `This action returns a #${id} file`;
-  }
-
-  update(id: number, updateFileDto: UpdateFileDto) {
-    return `This action updates a #${id} file`;
   }
 
   remove(id: number) {
