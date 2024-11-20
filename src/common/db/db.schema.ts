a111911373da19c3e3b35953c176b2d775eacbab
Marcello Coelho <marcellogcoelho@outlook.com>
2024-11-20 23:02:12 +0000

    feat: add file origin and type enums to files entity for enhanced data categorization

--- src/common/db/db.schema.ts
+++ src/common/db/db.schema.ts
@@ -5,13 +5,15 @@ import {
   userOrganizationsRelations,
 } from "@/resources/organizations/entities/organizations.entity";
 
-import { files } from "@/resources/files/entities/file.entity";
+import { files, fileOriginEnum, fileTypeEnum } from "@/resources/files/entities/file.entity";
 
 import { users, usersRelations } from "@/resources/users/entities/user.entity";
 
 export {
   users,
   files,
+  fileOriginEnum,
+  fileTypeEnum,
   organizations,
   userOrganizations,
   usersRelations,
--- src/resources/files/entities/file.entity.ts
+++ src/resources/files/entities/file.entity.ts
@@ -1,6 +1,28 @@
 import { organizations, users } from "@/common/db/db.schema";
 import { InferSelectModel } from "drizzle-orm";
-import { pgTable, serial, text, timestamp, uuid } from "drizzle-orm/pg-core";
+import { pgEnum, pgTable, serial, text, timestamp, uuid } from "drizzle-orm/pg-core";
+
+export enum FileOrigin {
+  DIGITIZED = "DIGITIZED",
+  DIGITAL = "DIGITAL",
+}
+
+export enum FileType {
+  INVOICE = "INVOICE",
+  RECEIPT = "RECEIPT",
+  WORK_ORDER = "WORK_ORDER",
+  BILL = "BILL",
+  SICK_NOTE = "SICK_NOTE",
+}
+
+export const fileOriginEnum = pgEnum("file_origin", [FileOrigin.DIGITIZED, FileOrigin.DIGITAL]);
+export const fileTypeEnum = pgEnum("file_type", [
+  FileType.INVOICE,
+  FileType.RECEIPT,
+  FileType.WORK_ORDER,
+  FileType.BILL,
+  FileType.SICK_NOTE,
+]);
 
 export const files = pgTable("files", {
   id: serial("id").primaryKey(),
@@ -11,6 +33,8 @@ export const files = pgTable("files", {
   organization_id: uuid("organization_id")
     .notNull()
     .references(() => organizations.id),
+  file_origin: fileOriginEnum(),
+  file_type: fileTypeEnum(),
   created_at: timestamp("created_at").defaultNow(),
   updated_at: timestamp("updated_at").defaultNow(),
 });
