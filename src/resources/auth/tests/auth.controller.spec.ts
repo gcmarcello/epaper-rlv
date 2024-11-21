8838e96789a3a4da6f169232ea6e5bcaec48a59d
Marcello Coelho <marcellogcoelho@outlook.com>
2024-11-21 20:21:15 +0000

    test: enhance AuthController and AuthService tests with comprehensive login and organization update scenarios

--- src/resources/auth/tests/auth.controller.spec.ts
+++ src/resources/auth/tests/auth.controller.spec.ts
@@ -1,18 +1,164 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { AuthController } from "../auth.controller";
+import { AuthService } from "../auth.service";
+import { JwtService } from "@nestjs/jwt";
+import { ConfigService } from "@nestjs/config";
+import { LoginDto } from "../dto/login.dto";
+import { User } from "@/resources/users/entities/user.entity";
+import { TsRestException } from "@ts-rest/nest";
+import { authContract } from "@/resources/auth/auth.contract";
+import { AuthenticatedRequest } from "@/types/authenticatedRequest";
+
+export const userMock = {
+  id: "uuid",
+  created_at: new Date(),
+  updated_at: new Date(),
+  email: "email@email.com",
+  name: "name",
+} as User;
+
+export const AuthServiceMock = {
+  provide: AuthService,
+  useValue: {
+    login: jest.fn(),
+    updateActiveOrganization: jest.fn(),
+  },
+};
+
+export const ConfigServiceMock = {
+  provide: ConfigService,
+  useValue: {
+    get: jest.fn().mockReturnValue("secret"),
+  },
+};
+
+export const JwtServiceMock = {
+  provide: JwtService,
+  useValue: {
+    sign: jest.fn().mockReturnValue("token"),
+  },
+};
 
 describe("AuthController", () => {
-  let controller: AuthController;
+  let authController: AuthController;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
+    const moduleFixture: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
+      providers: [AuthServiceMock, ConfigServiceMock, JwtServiceMock],
     }).compile();
 
-    controller = module.get<AuthController>(AuthController);
+    authController = moduleFixture.get<AuthController>(AuthController);
   });
 
   it("should be defined", () => {
-    expect(controller).toBeDefined();
+    expect(authController).toBeDefined();
+  });
+
+  describe("login", () => {
+    it("should return a token", async () => {
+      const loginDto: LoginDto = { email: "email", password: "password" };
+      const tokenResponse = { token: "token" };
+
+      const handler = authController["login"]();
+
+      jest.spyOn(AuthServiceMock.useValue, "login").mockResolvedValue(tokenResponse);
+
+      const result = await handler({
+        body: loginDto,
+        headers: {
+          "content-type": "application/json",
+        },
+      });
+
+      expect(result).toEqual({
+        status: 200,
+        body: tokenResponse,
+      });
+    });
+    it("should return a 401 error", async () => {
+      const loginDto: LoginDto = { email: "email", password: "password" };
+
+      const exception = new TsRestException(authContract.login, {
+        status: 401,
+        body: { message: "Senha ou usuário incorretos." },
+      });
+
+      jest.spyOn(AuthServiceMock.useValue, "login").mockRejectedValue(exception);
+
+      const handler = authController["login"]();
+
+      await expect(
+        handler({
+          body: loginDto,
+          headers: {
+            "content-type": "application/json",
+          },
+        })
+      ).rejects.toThrow(exception);
+    });
+  });
+
+  describe("updateActiveOrganization", () => {
+    it("should return a token", async () => {
+      const tokenResponse = { token: "token" };
+
+      const mockedRequest = {
+        user: {
+          id: "uuid",
+          name: "name",
+          organizationId: "uuid",
+        },
+      } as AuthenticatedRequest;
+
+      const handler = authController["updateActiveOrganization"](mockedRequest);
+
+      jest
+        .spyOn(AuthServiceMock.useValue, "updateActiveOrganization")
+        .mockResolvedValue(tokenResponse);
+
+      const result = await handler({
+        headers: {
+          "content-type": "application/json",
+        },
+        body: {
+          organizationId: "uuid",
+        },
+      });
+
+      expect(result).toEqual({
+        status: 200,
+        body: tokenResponse,
+      });
+    });
+    it("should throw a 403", async () => {
+      const mockedRequest = {
+        user: {
+          id: "uuid",
+          name: "name",
+          organizationId: "uuid",
+        },
+      } as AuthenticatedRequest;
+
+      const exception = new TsRestException(authContract.updateActiveOrganization, {
+        status: 403,
+        body: { message: "Usuário não é membro da organização." },
+      });
+
+      const handler = authController["updateActiveOrganization"](mockedRequest);
+
+      jest.spyOn(AuthServiceMock.useValue, "updateActiveOrganization").mockRejectedValue(exception);
+
+      await expect(
+        handler({
+          headers: {
+            "content-type": "application/json",
+          },
+          body: {
+            organizationId: "uuid",
+          },
+        })
+      ).rejects.toThrow(exception);
+    });
   });
 });
--- src/resources/auth/tests/auth.service.spec.ts
+++ src/resources/auth/tests/auth.service.spec.ts
@@ -1,18 +1,117 @@
-import { Test, TestingModule } from "@nestjs/testing";
 import { AuthService } from "../auth.service";
+import { JwtService } from "@nestjs/jwt";
+import { LoginDto } from "../dto/login.dto";
+import { TsRestException } from "@ts-rest/nest";
+import { organizationContract } from "@/resources/organizations/organizations.contract";
+import { compare } from "@/utils/bcrypt";
+import { UserPayload } from "@/types/authenticatedRequest";
+import { NodePgDatabase } from "drizzle-orm/node-postgres";
+import * as schema from "../../../common/db/db.schema";
+
+jest.mock("@/utils/bcrypt", () => ({
+  compare: jest.fn(),
+}));
 
 describe("AuthService", () => {
   let service: AuthService;
+  let jwtService: JwtService;
+  let db: NodePgDatabase<typeof schema>;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
-    }).compile();
-
-    service = module.get<AuthService>(AuthService);
+    db = {
+      query: {
+        users: { findFirst: jest.fn() },
+        userOrganizations: { findFirst: jest.fn() },
+      },
+    } as any;
+    jwtService = new JwtService();
+    service = new AuthService(db, jwtService);
   });
 
   it("should be defined", () => {
     expect(service).toBeDefined();
+  });
+
+  describe("login", () => {
+    it("should throw an error if user is not found", async () => {
+      (db.query.users.findFirst as jest.Mock).mockResolvedValue(null);
+
+      const loginDto: LoginDto = { email: "test@example.com", password: "password" };
+
+      await expect(service.login(loginDto)).rejects.toThrow(
+        new TsRestException(organizationContract.findOrgs, {
+          status: 404,
+          body: { message: "Senha ou usuário incorretos." },
+        })
+      );
+    });
+
+    it("should throw an error if password does not match", async () => {
+      (db.query.users.findFirst as jest.Mock).mockResolvedValue({
+        id: 1,
+        email: "test@example.com",
+        password: "hashedpassword",
+      });
+
+      (compare as jest.Mock).mockResolvedValue(false);
+
+      const loginDto: LoginDto = { email: "test@example.com", password: "password" };
+
+      await expect(service.login(loginDto)).rejects.toThrow(
+        new TsRestException(organizationContract.findOrgs, {
+          status: 404,
+          body: { message: "Senha ou usuário incorretos." },
+        })
+      );
+    });
+
+    it("should return a token if login is successful", async () => {
+      (db.query.users.findFirst as jest.Mock).mockResolvedValue({
+        id: 1,
+        email: "test@example.com",
+        password: "hashedpassword",
+      });
+
+      (compare as jest.Mock).mockResolvedValue(true);
+      jest.spyOn(jwtService, "signAsync").mockResolvedValue("token");
+
+      const loginDto: LoginDto = { email: "test@example.com", password: "password" };
+
+      const result = await service.login(loginDto);
+
+      expect(result).toEqual({ token: "token" });
+    });
+  });
+
+  describe("updateActiveOrganization", () => {
+    it("should throw an error if user is not a member of the organization", async () => {
+      (db.query.userOrganizations.findFirst as jest.Mock).mockResolvedValue(null);
+
+      const payload: UserPayload = { id: "uuid", name: "Test User" };
+      const organizationId = "org1";
+
+      await expect(service.updateActiveOrganization(payload, organizationId)).rejects.toThrow(
+        new TsRestException(organizationContract.findOrgs, {
+          status: 403,
+          body: { message: "Usuário não é membro da organização." },
+        })
+      );
+    });
+
+    it("should return a token if user is a member of the organization", async () => {
+      (db.query.userOrganizations.findFirst as jest.Mock).mockResolvedValue({
+        user_id: 1,
+        organization_id: "org1",
+      });
+
+      jest.spyOn(jwtService, "signAsync").mockResolvedValue("token");
+
+      const payload: UserPayload = { id: "uuid", name: "Test User" };
+      const organizationId = "org1";
+
+      const result = await service.updateActiveOrganization(payload, organizationId);
+
+      expect(result).toEqual({ token: "token" });
+    });
   });
 });
