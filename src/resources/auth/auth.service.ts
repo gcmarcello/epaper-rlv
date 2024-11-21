40cb4e2ef6c6df1d0ca797e7984cc49b25b4d64e
Marcello Coelho <marcellogcoelho@outlook.com>
2024-11-21 14:39:10 +0000

    fix: update import paths for DB module and utilities to use aliasing

--- src/resources/auth/auth.service.ts
+++ src/resources/auth/auth.service.ts
@@ -1,12 +1,12 @@
 import { Inject, Injectable } from "@nestjs/common";
 import { and, eq } from "drizzle-orm";
 import { NodePgDatabase } from "drizzle-orm/node-postgres";
-import { DrizzleAsyncProvider } from "src/common/db/db.provider";
-import * as schema from "src/common/db/db.schema";
+import { DrizzleAsyncProvider } from "../../common/db/db.provider";
+import * as schema from "../../common/db/db.schema";
 import { LoginDto } from "./dto/login.dto";
 import { TsRestException } from "@ts-rest/nest";
 import { organizationContract } from "../organizations/organizations.contract";
-import { compare } from "src/utils/bcrypt";
+import { compare } from "@/utils/bcrypt";
 import { JwtService } from "@nestjs/jwt";
 import { UserPayload } from "@/types/authenticatedRequest";
 
