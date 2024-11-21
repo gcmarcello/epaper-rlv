e6e101cb715e19638ea1a1721f8dca170662d607
Marcello Coelho <marcellogcoelho@outlook.com>
2024-11-21 21:09:20 +0000

    test: enhance OrganizationsController and OrganizationsService tests with additional scenarios and mocks

--- src/resources/organizations/tests/organizations.controller.spec.ts
+++ src/resources/organizations/tests/organizations.controller.spec.ts
@@ -1,6 +1,30 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { OrganizationsController } from "../organizations.controller";
 import { OrganizationsService } from "../organizations.service";
+import { ConfigService } from "@nestjs/config";
+import { JwtService } from "@nestjs/jwt";
+import { CreateOrganizationDto } from "../dto/create-organization.dto";
+import { AuthenticatedRequest } from "@/types/authenticatedRequest";
+import { Organization } from "../entities/organizations.entity";
+
+export const ConfigServiceMock = {
+  provide: ConfigService,
+  useValue: {},
+};
+
+export const JwtServiceMock = {
+  provide: JwtService,
+  useValue: {},
+};
+
+export const OrganizationsServiceMock = {
+  provide: OrganizationsService,
+  useValue: {
+    create: jest.fn(),
+    findAll: jest.fn(),
+    findbyId: jest.fn(),
+  },
+};
 
 describe("OrganizationsController", () => {
   let controller: OrganizationsController;
@@ -8,7 +32,7 @@ describe("OrganizationsController", () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [OrganizationsController],
-      providers: [OrganizationsService],
+      providers: [OrganizationsServiceMock, JwtServiceMock, ConfigServiceMock],
     }).compile();
 
     controller = module.get<OrganizationsController>(OrganizationsController);
@@ -16,5 +40,108 @@ describe("OrganizationsController", () => {
 
   it("should be defined", () => {
     expect(controller).toBeDefined();
+  });
+
+  describe("create", () => {
+    it("should return org created", async () => {
+      const organizationDto: CreateOrganizationDto = { name: "Org Test" };
+
+      const mockedRequest = {
+        user: {
+          id: "uuid",
+          name: "name",
+        },
+      } as AuthenticatedRequest;
+
+      const mockedNewOrg: Organization = {
+        created_at: new Date(),
+        id: "uuid",
+        name: "Org Test",
+        updated_at: new Date(),
+        owner_id: "uuid",
+      };
+
+      const handler = controller["create"](mockedRequest);
+
+      jest.spyOn(OrganizationsServiceMock.useValue, "create").mockResolvedValue(mockedNewOrg);
+
+      const result = await handler({
+        headers: {
+          "content-type": "application/json",
+        },
+        body: organizationDto,
+      });
+
+      expect(result).toEqual({
+        status: 200,
+        body: mockedNewOrg,
+      });
+    });
+  });
+
+  describe("findAll", () => {
+    it("should return all orgs", async () => {
+      const mockedResponse = {
+        organizations: [
+          {
+            created_at: new Date(),
+            id: "uuid",
+            name: "Org Test",
+            updated_at: new Date(),
+            owner_id: "uuid",
+          },
+        ],
+        total: 1,
+      };
+
+      const handler = controller["findAll"]();
+
+      jest.spyOn(OrganizationsServiceMock.useValue, "findAll").mockResolvedValue(mockedResponse);
+
+      const result = await handler({
+        query: {
+          limit: 10,
+          offset: 0,
+        },
+        headers: {
+          "content-type": "application/json",
+        },
+      });
+
+      expect(result).toEqual({
+        status: 200,
+        body: mockedResponse,
+      });
+    });
+  });
+
+  describe("findbyId", () => {
+    it("should return org by id", async () => {
+      const mockedOrg: Organization = {
+        created_at: new Date(),
+        id: "uuid",
+        name: "Org Test",
+        updated_at: new Date(),
+        owner_id: "uuid",
+      };
+
+      const handler = controller["findbyId"]();
+
+      jest.spyOn(OrganizationsServiceMock.useValue, "findbyId").mockResolvedValue(mockedOrg);
+
+      const result = await handler({
+        params: {
+          id: "uuid",
+        },
+        headers: {
+          "content-type": "application/json",
+        },
+      });
+
+      expect(result).toEqual({
+        status: 200,
+        body: mockedOrg,
+      });
+    });
   });
 });
--- src/resources/organizations/tests/organizations.service.spec.ts
+++ src/resources/organizations/tests/organizations.service.spec.ts
@@ -1,18 +1,106 @@
-import { Test, TestingModule } from "@nestjs/testing";
 import { OrganizationsService } from "../organizations.service";
+import * as schema from "../../../common/db/db.schema";
+import { NodePgDatabase } from "drizzle-orm/node-postgres";
+import { Organization } from "../entities/organizations.entity";
+import { TsRestException } from "@ts-rest/nest";
+import { organizationContract } from "../organizations.contract";
 
 describe("OrganizationsService", () => {
   let service: OrganizationsService;
+  let db: NodePgDatabase<typeof schema>;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [OrganizationsService],
-    }).compile();
+    db = {
+      transaction: jest.fn(),
+    } as any;
 
-    service = module.get<OrganizationsService>(OrganizationsService);
+    service = new OrganizationsService(db);
   });
 
   it("should be defined", () => {
     expect(service).toBeDefined();
+  });
+
+  describe("create", () => {
+    it("should create an organization", async () => {
+      const organizationDto = {
+        name: "Organization",
+      };
+
+      const owner_id = crypto.randomUUID();
+      const id = crypto.randomUUID();
+
+      const createdOrg: Organization = {
+        id,
+        created_at: new Date(),
+        name: organizationDto.name,
+        updated_at: new Date(),
+        owner_id,
+      };
+
+      jest.spyOn(service, "create").mockResolvedValue(createdOrg);
+
+      const result = await service.create(organizationDto, owner_id);
+
+      expect(result).toEqual(createdOrg);
+    });
+  });
+  describe("findAll", () => {
+    it("should return all organizations", async () => {
+      const mockedResponse = {
+        organizations: [
+          {
+            created_at: new Date(),
+            id: "uuid",
+            name: "Org Test",
+            updated_at: new Date(),
+            owner_id: "uuid",
+          },
+        ],
+        total: 1,
+      };
+
+      jest.spyOn(service, "findAll").mockResolvedValue(mockedResponse);
+
+      const result = await service.findAll();
+
+      expect(result).toEqual(mockedResponse);
+    });
+  });
+  describe("findById", () => {
+    it("should return an organization by id", async () => {
+      const id = crypto.randomUUID();
+      const mockedOrg = {
+        id,
+        created_at: new Date(),
+        name: "Test Org",
+        updated_at: new Date(),
+        owner_id: crypto.randomUUID(),
+      };
+
+      jest.spyOn(service, "findbyId").mockResolvedValue(mockedOrg);
+
+      const result = await service.findbyId(id);
+
+      expect(result).toEqual(mockedOrg);
+    });
+
+    it("should throw exception if organization not found", async () => {
+      const id = crypto.randomUUID();
+
+      jest.spyOn(service, "findbyId").mockRejectedValue(
+        new TsRestException(organizationContract.findOrg, {
+          status: 404,
+          body: { message: "No User Found" },
+        })
+      );
+
+      await expect(service.findbyId(id)).rejects.toEqual(
+        new TsRestException(organizationContract.findOrg, {
+          status: 404,
+          body: { message: "No User Found" },
+        })
+      );
+    });
   });
 });
