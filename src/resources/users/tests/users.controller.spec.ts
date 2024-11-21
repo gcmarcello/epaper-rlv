d75086ee06c014d072dab77bb664ba46b5f3fd12
Marcello Coelho <marcellogcoelho@outlook.com>
2024-11-21 22:26:02 +0000

    test: enhance UsersController and UsersService tests with additional scenarios and mocks

--- src/resources/users/tests/users.controller.spec.ts
+++ src/resources/users/tests/users.controller.spec.ts
@@ -1,20 +1,114 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { UsersController } from "../users.controller";
 import { UsersService } from "../users.service";
+import { ConfigModule } from "@nestjs/config";
+import { DBModule } from "@/common/db/db.module";
+import { JwtService } from "@nestjs/jwt";
+import { AuthGuard } from "../../auth/auth.guard";
+import { User } from "../entities/user.entity";
+import { TsRestException } from "@ts-rest/nest";
+import { userContract } from "../users.contract";
 
 describe("UsersController", () => {
   let controller: UsersController;
+  let usersService: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [DBModule, ConfigModule],
       controllers: [UsersController],
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        JwtService,
+        {
+          provide: AuthGuard,
+          useValue: {
+            canActivate: jest.fn(() => true),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
+    usersService = module.get<UsersService>(UsersService);
   });
 
   it("should be defined", () => {
     expect(controller).toBeDefined();
+  });
+
+  describe("create", () => {
+    it("should create a user and return status 200", async () => {
+      const body = { name: "John Doe", email: "email@email.com", password: "password" };
+      const id = crypto.randomUUID();
+
+      jest.spyOn(usersService, "create").mockResolvedValue({
+        user: { id, name: body.name },
+      });
+
+      const result = await controller.create()({ body, headers: {} });
+
+      expect(result).toEqual({ status: 200, body: { user: { name: body.name, id } } });
+    });
+
+    it("should return status 409 if user creation fails", async () => {
+      const body = { name: "John Doe", email: "email@email.com", password: "password" };
+
+      jest.spyOn(usersService, "create").mockRejectedValue(
+        new TsRestException(userContract.createUser, {
+          status: 409,
+          body: { message: "Email already exists" },
+        })
+      );
+
+      await expect(controller.create()({ body, headers: {} })).rejects.toThrow(
+        new TsRestException(userContract.createUser, {
+          status: 409,
+          body: { message: "Email already exists" },
+        })
+      );
+    });
+  });
+
+  describe("findAll", () => {
+    it("should return all users with status 200", async () => {
+      const query = { limit: 10, offset: 0 };
+      const users: User[] = [
+        {
+          id: crypto.randomUUID(),
+          name: "John Doe",
+          created_at: new Date(),
+          updated_at: new Date(),
+          email: "email@email.com",
+          password: "password",
+        },
+      ];
+      const total = users.length;
+      jest.spyOn(usersService, "findAll").mockResolvedValue({ users, total });
+
+      const result = await controller.findAll()({ query, headers: {} });
+
+      expect(result).toEqual({ status: 200, body: { users, total } });
+    });
+  });
+
+  describe("findbyId", () => {
+    it("should return a user by id with status 200", async () => {
+      const id = crypto.randomUUID();
+      const params = { id };
+      const user = {
+        id,
+        name: "John Doe",
+        created_at: new Date(),
+        updated_at: new Date(),
+        email: "email@email.com",
+        password: "password",
+      };
+      jest.spyOn(usersService, "findbyId").mockResolvedValue(user);
+
+      const result = await controller.findbyId()({ params, headers: {} });
+
+      expect(result).toEqual({ status: 200, body: user });
+    });
   });
 });
--- src/resources/users/tests/users.service.spec.ts
+++ src/resources/users/tests/users.service.spec.ts
@@ -1,18 +1,135 @@
-import { Test, TestingModule } from "@nestjs/testing";
 import { UsersService } from "../users.service";
+import { CreateUserDto } from "../dto/create-user.dto";
+import { NodePgDatabase } from "drizzle-orm/node-postgres";
+import * as schema from "../../../common/db/db.schema";
+import { hash } from "@/utils/bcrypt";
+import { TsRestException } from "@ts-rest/nest";
+
+jest.mock("@/utils/bcrypt", () => ({
+  hash: jest.fn(),
+}));
 
 describe("UsersService", () => {
   let service: UsersService;
+  let db: NodePgDatabase<typeof schema>;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
-    }).compile();
+    db = {
+      query: {
+        users: {
+          findFirst: jest.fn(),
+          findMany: jest.fn(),
+        },
+      },
+      insert: jest.fn(),
+    } as any;
 
-    service = module.get<UsersService>(UsersService);
+    service = new UsersService(db);
   });
 
   it("should be defined", () => {
     expect(service).toBeDefined();
+  });
+
+  describe("create", () => {
+    it("should create a new user", async () => {
+      const createUserDto: CreateUserDto = {
+        email: "test@example.com",
+        password: "password",
+        name: "Test User",
+      };
+
+      jest
+        .spyOn(service, "create")
+        .mockResolvedValueOnce({ user: { name: createUserDto.name, id: "uuid" } });
+      (hash as jest.Mock).mockResolvedValue("hashedpassword");
+
+      const data = { ...createUserDto, password: await hash("password") };
+
+      const result = await service.create(data);
+      expect(result).toEqual({ user: { name: createUserDto.name, id: "uuid" } });
+    });
+    it("should throw an error if email already exists", async () => {
+      const createUserDto: CreateUserDto = {
+        email: "test@example.com",
+        password: "password",
+        name: "Test User",
+      };
+
+      (db.query.users.findFirst as jest.Mock).mockResolvedValueOnce({ email: createUserDto.email });
+
+      await expect(service.create(createUserDto)).rejects.toThrow(
+        new TsRestException(expect.anything(), {
+          status: 409,
+          body: { message: "Email already exists" },
+        })
+      );
+    });
+  });
+
+  describe("findAll", () => {
+    it("should return all users", async () => {
+      const users = [
+        {
+          email: "test1@example.com",
+          id: "1",
+          name: "Test User 1",
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          email: "test2@example.com",
+          id: "2",
+          name: "Test User 2",
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+      ];
+
+      (db.query.users.findMany as jest.Mock).mockResolvedValueOnce(users);
+
+      const result = await service.findAll();
+      expect(result).toEqual({ users, total: users.length });
+    });
+
+    it("should throw an error if no users found", async () => {
+      (db.query.users.findMany as jest.Mock).mockResolvedValueOnce([]);
+
+      await expect(service.findAll()).rejects.toThrow(
+        new TsRestException(expect.anything(), {
+          status: 404,
+          body: { message: "No Users Found" },
+        })
+      );
+    });
+  });
+
+  describe("findbyId", () => {
+    it("should return a user by id", async () => {
+      const userId = crypto.randomUUID();
+      const user = {
+        email: "test@example.com",
+        id: userId,
+        name: "Test User",
+        created_at: new Date(),
+        updated_at: new Date(),
+      };
+
+      (db.query.users.findFirst as jest.Mock).mockResolvedValueOnce(user);
+
+      const result = await service.findbyId(userId);
+      expect(result).toEqual(user);
+    });
+
+    it("should throw an error if user not found", async () => {
+      (db.query.users.findFirst as jest.Mock).mockResolvedValueOnce(null);
+
+      await expect(service.findbyId(crypto.randomUUID())).rejects.toThrow(
+        new TsRestException(expect.anything(), {
+          status: 404,
+          body: { message: "No User Found" },
+        })
+      );
+    });
   });
 });
